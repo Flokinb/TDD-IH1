65f2dd265059c865c40867b6a5edc4d9d310053e
Flokinb <112621560+Flokinb@users.noreply.github.com>
2023-02-26 23:30:07 +0000

    Update sample.py

---
+++ sample.py
@@ -0,0 +1,47 @@
+taxes = {
+    'South Korea': 0.1,
+    'Australia': 0.1,
+    'China': 0.13,
+    'United Kingdom': 0.2,
+    'Germany': 0.19,
+    'France': 0.2
+}
+
+shipping = {
+    'South Korea': 5.9,
+    'Australia': 5.7,
+    'China': 5.1,
+    'United Kingdom': 4.2,
+    'Germany': 4.3,
+    'France': 4.9
+}
+
+def calculate_total_price(items, location):
+    return calculate_base_price(items) + calculate_taxes(items, location) + calculate_shipping_fees(items, location)
+
+def calculate_base_price(items):
+    total = 0
+    for item in items:
+        price = item["Price"]
+        quantity = item["Quantity"]
+        total += price * quantity
+    return round(total, 2)
+
+
+def calculate_taxes(items, location):
+    total_taxes = 0
+    for item in items:
+        price = item["Price"]
+        quantity = item["Quantity"]
+        tax_rate = taxes[location]
+        total_taxes += price * quantity * tax_rate
+    return round(total_taxes, 2)
+
+def calculate_shipping_fees(items, location):
+    total_fees = 0
+    for item in items:
+        weight = item["Weight"]
+        quantity = item["Quantity"]
+        shipping_rate = shipping[location]
+        total_fees += weight * quantity * shipping_rate
+    return round(total_fees, 2)
